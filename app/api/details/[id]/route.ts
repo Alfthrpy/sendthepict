4d3c10ec2583f02324d869e4b76ad069757c80b7
Al-Fathir <118950136+Alfthrpy@users.noreply.github.com>
2024-11-05 17:09:48 +0700

    fix : error build

--- app/api/details/[id]/route.ts
+++ app/api/details/[id]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(req : Request,{ params }: { params: { id: string } }) {
     const id =  params.id;
     try {
         const response = await prisma.message.findUnique({
